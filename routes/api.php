96043ed7033260db85f2be11de73542d707e81e4
Khanhdq <quockhanhvip98@gmail.com>
2025-06-27 16:29:29 +0700

    test_cicd

--- routes/api.php
+++ routes/api.php
@@ -30,7 +30,7 @@ Route::prefix('auth')->group(function(){
 });
 
 Route::middleware(['auth:sanctum'])->group(function(){
-    // User APIss
+    // User APIs
     Route::post('/user', [UserController::class, 'store']);
 
     // Blog APIs;
